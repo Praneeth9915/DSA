eb8fc245eb3478f758dd701dd452dd819f680270
Ram Praneeth <73172364+Praneeth9915@users.noreply.github.com>
2026-06-30 00:03:21 +0530

    Time: 15 ms (12.52%) | Memory: 27.2 MB (7.96%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -4,7 +4,22 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-
         k %= n
 
-        nums[:] = nums[-k:] + nums[:-k]
+        count = 0
+        start = 0
+
+        while count < n:
+            current = start
+            prev = nums[start]
+
+            while True:
+                next_index = (current + k) % n
+                nums[next_index], prev = prev, nums[next_index]
+                current = next_index
+                count += 1
+
+                if current == start:
+                    break
+
+            start += 1
