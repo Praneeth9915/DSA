ad9b712fc7d100d5862e835e16c142b5f67266ad
Ram Praneeth <73172364+Praneeth9915@users.noreply.github.com>
2026-07-01 23:46:05 +0530

    Time: 3 ms (89.42%) | Memory: 22.6 MB (94.45%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,17 +5,11 @@ class Solution:
         for num in nums:
             freq[num] = freq.get(num, 0) + 1
 
-        bucket = [[] for _ in range(len(nums) + 1)]
-
-        for num, count in freq.items():
-            bucket[count].append(num)
+        sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
 
         ans = []
 
-        for i in range(len(bucket) - 1, 0, -1):
+        for i in range(k):
+            ans.append(sorted_freq[i][0])
 
-            for num in bucket[i]:
-                ans.append(num)
-
-                if len(ans) == k:
-                    return ans
+        return ans
